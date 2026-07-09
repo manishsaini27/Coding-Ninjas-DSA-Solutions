59d9f2d2818b5f7588af57bd6c3409d17b2e563d
Manish Saini <saini36785@gmail.com>
2026-07-09 09:47:42 +0530

    Added pivote element in array

---
+++ binary_search/pivoteElementINsortedarray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+int getPivot(int arr[],int n ){
+    int s =0 ;
+    int e = n-1;
+    int mid  = s+(e-s)/2;
+    while(s<e) {
+        if(arr[mid] > arr[0] ){
+            s = mid+1;
+        }else {
+            e  = mid;
+        }
+        mid  = s+(e-s)/2;
+    }
+    return s;
+}
+
+int main (){
+    int arr[6] = {4,5,6,1,3};
+    cout<<"pivot element " << getPivot(arr,6);
+}
