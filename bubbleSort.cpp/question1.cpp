9af345926ed35987afd2aed05182e170e1491769
Manish Saini <saini36785@gmail.com>
2026-07-24 18:22:07 +0530

    added insertion question

--- bubbleSort.cpp/question1.cpp
+++ bubbleSort.cpp/question1.cpp
@@ -1,11 +1,18 @@
-#include <bits/stdc++.h> 
-void bubbleSort(vector<int>& arr, int n)
-{   
-    for(int i=1;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1]){
+#include<iostream>
+using namespace std ;
+int bubblesort(int arr[] , int n) {
+    for(int i =1; i<n;i++) {
+        for(int j=0; j<n-i; j++) {
+            if(arr[j]> arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
+    }
+}
+int main() {
+    int arr[5] = {5,4,3,2,1};
+    bubblesort(arr,5);
+    for(int i=0 ; i<5; i++){
+        cout<< arr[i] << endl;
     }
 }
---
+++ insertionSort/ques1.md
@@ -0,0 +1,23 @@
+You are given ‘N’ integers in the form of an array ‘ARR’. Print the sorted array using the insertion sort.
+
+Note :
+No need to return anything. You should sort the array in-place.
+For example :
+Let ‘ARR’ be: [1, 4, 2]
+The sorted array will be: [1, 2, 4].
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= T <= 10
+1 <= N <= 5*10^3
+1 <= ARR[i] <= 10^5
+
+Time Limit: 1 sec
+Sample Input 1 :
+2
+4
+3 1 2 2
+3
+1 4 2
+Sample Output 1 :
+1 2 2 3
+1 2 4
